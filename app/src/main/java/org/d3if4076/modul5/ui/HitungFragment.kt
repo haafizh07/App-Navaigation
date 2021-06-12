249d2eb8228830cfba606246e1ee5b51f2f8a145
haafizh <haafizhhamda@student.telkomuniversity.ac.id>
2021-06-13 04:03:50 +0700

    Membuat navigasi ke app lain

--- app/src/main/java/org/d3if4076/modul5/ui/HitungFragment.kt
+++ app/src/main/java/org/d3if4076/modul5/ui/HitungFragment.kt
@@ -1,5 +1,6 @@
 package org.d3if4076.modul5.ui
 
+import android.content.Intent
 import android.os.Bundle
 import android.text.TextUtils
 import android.view.*
@@ -27,7 +28,7 @@ class HitungFragment : Fragment() {
                 HitungFragmentDirections.actionHitungFragmentToSaranFragment(kategoriBmi)
             )
         }
-
+        binding.shareButton.setOnClickListener { shareData() }
         setHasOptionsMenu(true)
         return binding.root
 
@@ -59,9 +60,30 @@ class HitungFragment : Fragment() {
 
         binding.bmiTextView.text = getString(R.string.bmi_x, bmi)
         binding.kategoriTextView.text = getString(R.string.kategori_x, kategori)
-        binding.saranButton.visibility = View.VISIBLE
+        binding.buttonGroup.visibility = View.VISIBLE
 
     }
+    private fun shareData() {
+        val selectedId = binding.radioGroup.checkedRadioButtonId
+        val gender = if (selectedId == R.id.priaRadioButton)
+            getString(R.string.pria)
+        else
+            getString(R.string.wanita)
+        val message = getString(R.string.bagikan_template,
+            binding.beratEditText.text,
+            binding.tinggiEditText.text,
+            gender,
+            binding.bmiTextView.text,
+            binding.kategoriTextView.text
+        )
+        val shareIntent = Intent(Intent.ACTION_SEND)
+        shareIntent.setType("text/plain").putExtra(Intent.EXTRA_TEXT, message)
+        if (shareIntent.resolveActivity(
+                requireActivity().packageManager) != null) {
+            startActivity(shareIntent)
+        }
+    }
+
 
     private fun getKategori(bmi: Float, isMale: Boolean): String {
         kategoriBmi = if (isMale) {
