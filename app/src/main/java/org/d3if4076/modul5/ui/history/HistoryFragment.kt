8a76404bd2b06e1f94524a51b567443c60fb6cdb
haafizh <haafizhhamda@student.telkomuniversity.ac.id>
2021-06-17 02:27:50 +0700

    Menambahkan fitur histori

---
+++ app/src/main/java/org/d3if4076/modul5/ui/history/HistoryFragment.kt
@@ -0,0 +1,18 @@
+package org.d3if4076.modul5.ui.history
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import org.d3if4076.modul5.databinding.FragmentHistoryBinding
+
+class HistoryFragment : Fragment() {
+    private lateinit var binding: FragmentHistoryBinding
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        binding = FragmentHistoryBinding.inflate(layoutInflater,
+            container, false)
+        return binding.root
+    }
+}
