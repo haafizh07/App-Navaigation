fbfa74238ef60d6901443e5dca3a4b01122d2283
haafizh <haafizhhamda@student.telkomuniversity.ac.id>
2021-06-17 04:07:24 +0700

    .Menampilkan data dalam bentuk list

--- app/src/main/java/org/d3if4076/modul5/ui/history/HistoryFragment.kt
+++ app/src/main/java/org/d3if4076/modul5/ui/history/HistoryFragment.kt
@@ -8,10 +8,13 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.observe
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.RecyclerView
 import org.d3if4076.modul5.databinding.FragmentHistoryBinding
 import org.d3if4076.modul5.db.BmiDb
 
 class HistoryFragment : Fragment() {
+    private lateinit var myAdapter: HistoryAdapter
     private val viewModel: HistoryViewModel by lazy {
         val db = BmiDb.getInstance(requireContext())
         val factory = HistoryViewModelFactory(db.dao)
@@ -23,12 +26,23 @@ class HistoryFragment : Fragment() {
                               savedInstanceState: Bundle?): View? {
         binding = FragmentHistoryBinding.inflate(layoutInflater,
             container, false)
+        myAdapter = HistoryAdapter()
+        with(binding.recyclerView) {
+            addItemDecoration(
+                DividerItemDecoration(context,
+                RecyclerView.VERTICAL)
+            )
+            adapter = myAdapter
+            setHasFixedSize(true)
+        }
         return binding.root
     }
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         viewModel.data.observe(viewLifecycleOwner) {
-            Log.d("HistoryFragment", "Jumlah data: ${it.size}")
+            binding.emptyView.visibility = if (it.isEmpty())
+                View.VISIBLE else View.GONE
+            myAdapter.updateData(it)
         }
     }
 
--- app/src/main/java/org/d3if4076/modul5/ui/hitung/HitungViewModel.kt
+++ app/src/main/java/org/d3if4076/modul5/ui/hitung/HitungViewModel.kt
@@ -8,6 +8,7 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import org.d3if4076.modul5.data.HasilBmi
+import org.d3if4076.modul5.data.HitungBmi
 import org.d3if4076.modul5.data.KategoriBmi
 import org.d3if4076.modul5.db.BmiDao
 import org.d3if4076.modul5.db.BmiEntity
@@ -16,30 +17,14 @@ class HitungViewModel(private val db: BmiDao) : ViewModel() {
     private val navigasi = MutableLiveData<KategoriBmi?>()
     private val hasilBmi = MutableLiveData<HasilBmi?>()
     fun hitungBmi(berat: String, tinggi: String, isMale: Boolean) {
-        val tinggiCm = tinggi.toFloat() / 100
-        val bmi = berat.toFloat() / (tinggiCm * tinggiCm)
-        val kategori = if (isMale) {
-            when {
-                bmi < 20.5 -> KategoriBmi.KURUS
-                bmi >= 27.0 -> KategoriBmi.GEMUK
-                else -> KategoriBmi.IDEAL
-            }
-        }
-        else {
-            when {
-                bmi < 18.5 -> KategoriBmi.KURUS
-                bmi >= 25.0 -> KategoriBmi.GEMUK
-                else -> KategoriBmi.IDEAL
-            }
-        }
-        hasilBmi.value = HasilBmi(bmi, kategori)
+        val dataBmi = BmiEntity(
+            berat = berat.toFloat(),
+            tinggi = tinggi.toFloat(),
+            isMale = isMale
+        )
+        hasilBmi.value = HitungBmi.hitung(dataBmi)
         viewModelScope.launch {
             withContext(Dispatchers.IO) {
-                val dataBmi = BmiEntity(
-                    berat = berat.toFloat(),
-                    tinggi = tinggi.toFloat(),
-                    isMale = isMale
-                )
                 db.insert(dataBmi)
             }
         }
